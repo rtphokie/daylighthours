7107eea97140e6e79ea0592260b4058e5fc383f6
rtphokie <q.i43,~sYTiebFrM>
2022-04-28 18:57:47 -0400

    calcs for Raleigh and Anchorage

--- daylighthours/daylighthours.py
+++ daylighthours/daylighthours.py
@@ -1,6 +1,8 @@
+import datetime
 from datetime import timedelta
 
 from dateutil.parser import parse
+from hijri_converter import Hijri, Gregorian
 from pytz import utc
 from skyfield import almanac
 from skyfield.api import Loader, wgs84
@@ -12,19 +14,23 @@ eph = load('de421.bsp')
 
 
 def daylighthours(lat, lng, startdate=None, enddate=None):
+    ''' calculates the differnce, in decimal days, between sunrise and sunset'''
     bluffton = wgs84.latlon(lat, lng)
 
     t0, t1 = timerange(enddate, startdate)
     t, y = almanac.find_discrete(t0, t1, almanac.sunrise_sunset(eph, bluffton))
 
     results = dict()
-    for i in range(0, len(t), 2):
+    if y[0] == 0:  # ensure we start with a sunrise
+        start = 1
+    else:
+        start = 0
+    for i in range(start, len(t), 2):
         if i == 0 and y[i] != 1:
-            continue # start with a sunrise
-        if i == len(t)-1 and y[i] != 0:
-            continue # end with a sunset
-        results[t[i+1].utc_strftime('%Y-%m-%d')]=t[i+1]-t[i]
-
+            continue  # start with a sunrise
+        if i == len(t) - 1 and y[i] != 0:
+            continue  # end with a sunset
+        results[t[i + 1].utc_strftime('%Y-%m-%d')] = t[i + 1] - t[i]
 
     #     chunk = ints[i:i + chunk_size]
     #     # process chunk of size <= chunk_size
@@ -46,3 +52,28 @@ def timerange(enddate, startdate):
         enddate_dt = parse(enddate)
         t1 = ts.from_datetime(enddate_dt.replace(tzinfo=utc))
     return t0, t1
+
+
+def ramadan_dates(year):
+    h = Gregorian(year, 1, 1).to_hijri()
+    if h.month > 9:
+        year = h.year - 1
+    else:
+        year = h.year
+    g = Hijri(year, 9, 1).to_gregorian()
+    start = datetime.date(g.year, g.month, g.day)
+    try:
+        g = Hijri(year, 9, 30).to_gregorian()
+    except:
+        g = Hijri(year, 9, 29).to_gregorian()
+    end = datetime.date(g.year, g.month, g.day)
+    return start, end
+
+
+def ramadan_daylight_hours(lat, lng, year):
+    start, end = ramadan_dates(year)
+    end += timedelta(days=1)  # avoid off by 1 error
+    hours = daylighthours(lat, lng, startdate=start.strftime('%x'), enddate=end.strftime('%x'))
+    tmin = timedelta(days=min(hours.values()))
+    tmax = timedelta(days=max(hours.values()))
+    return start, end, tmin, tmax
--- tests.py
+++ tests.py
@@ -1,13 +1,31 @@
 import unittest
-from daylighthours.daylighthours import daylighthours
 from pprint import pprint
+
+from daylighthours.daylighthours import daylighthours, ramadan_dates, ramadan_daylight_hours
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_daylighthours(self):
         result = daylighthours(35.78, -78.64, startdate='2022-01-01', enddate='2022-12-31')
         result = daylighthours(35.78, -78.64)
-        pprint(result)
+
+    def test_ramadan_start_end(self):
+        startdate, enddate = ramadan_dates(2022)
+        self.assertEqual(startdate.month, 4)
+        self.assertEqual(startdate.day, 2)
+        self.assertEqual(enddate.month, 5)
+        self.assertEqual(enddate.day, 1)
+
+    def test_ramadan_hours_raleigh(self):
+        for year in range(1980,2031):
+            start, end, daylighthoursmin, daylighthoursmax = ramadan_daylight_hours(35.78, -78.64, year)
+            print(year, start, end, daylighthoursmin, daylighthoursmax)
+
+
+    def test_ramadan_hours_alaska(self):
+        for year in range(1980,2031):
+            start, end, daylighthoursmin, daylighthoursmax = ramadan_daylight_hours(61.22, -149.90, year)
+            print(year, start, end, daylighthoursmin, daylighthoursmax)
 
 
 if __name__ == '__main__':
